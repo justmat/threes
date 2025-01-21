e82d08a344d1fabcd6b2d321dc5177f1542e8f7b
mat <mat@itmemat.com>
2025-01-21 14:31:10 -0500

    Update 16ng.lua

--- 16ng.lua
+++ 16ng.lua
@@ -27,7 +27,7 @@ grid = function(x, y, z)
 	grid_led_all(0)
 	local cc_val = math.floor(linlin(1, 16, 127, 1, y))
 	if z == 1 then
-		midi_cc(cc_numbers[x], cc_val, cc_chans[x])
+		midi_cc(cc_nums[x], cc_val, cc_chans[x])
 		last_press[x] = y
 	end
 	for i = 1, 16 do
